f69c3a4d90df0d1026f4a20a5628c889f8baa28e
castro32 <okanga.fidel@gmail.com>
2024-10-31 23:19:57 +0300

    plp assignment 1 python

---
+++ user_input.py
@@ -0,0 +1,8 @@
+print('Enter your name:')
+name=input()
+print('Enter your age:')
+age=input()
+print('Enter your location:')
+location=input()
+
+print("Hello, " + name + "I am " + age + " years old and live in "  + location + ".")
